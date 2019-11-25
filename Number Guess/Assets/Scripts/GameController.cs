bb60ac5ff458f0e9a23f829aef4732db494a8973
Juan Insuasti <juan.insuasti@gmail.com>
2019-11-25 10:36:07 -0500

    Guess my number game

---
+++ Number Guess/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    int min = 0;
+    int max = 100;
+    int guess;
+    [SerializeField] TextMeshProUGUI numberContainer;
+
+
+    private void Start()
+    {
+        guess = Random.Range(min, max);
+        printNumber();
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        guess = Random.Range(min, max);
+        printNumber();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        guess = Random.Range(min, max);
+        printNumber();
+    }
+
+    private void printNumber()
+    {
+        numberContainer.text = guess.ToString();
+    }
+}
